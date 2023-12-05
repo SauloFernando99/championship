c7152705b9e75691f2432be881d3c3a39dc0ed23
GiovaneGuimaraes <gioguima24@gmail.com>
2023-12-04 22:44:44 -0300

    alo alo marciano

--- championship/src/main/java/br/edu/ifsp/HelloApplication.java
+++ championship/src/main/java/br/edu/ifsp/HelloApplication.java
@@ -1,5 +1,6 @@
 package br.edu.ifsp;
 
+import br.edu.ifsp.domain.entities.team.Team;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -18,6 +19,5 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
     }
 }
