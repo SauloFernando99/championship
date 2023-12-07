e4efcfdb5670094444fd1deeeed630855d821102
SauloFernando99 <saulofernando114@gmail.com>
2023-12-06 21:00:35 -0300

    feat: use cases to administrate matches results and scores

--- championship/src/main/java/br/edu/ifsp/domain/usecases/match/administration/FinishMatchUseCase.java
+++ championship/src/main/java/br/edu/ifsp/domain/usecases/match/administration/FinishMatchUseCase.java
@@ -1,2 +1,28 @@
-package br.edu.ifsp.domain.usecases.match.administration;public class FinishMatchUseCase {
+package br.edu.ifsp.domain.usecases.match.administration;
+
+import br.edu.ifsp.domain.entities.championship.Match;
+import br.edu.ifsp.domain.usecases.match.crud.MatchDAO;
+import br.edu.ifsp.domain.usecases.utils.EntityNotFoundException;
+
+public class FinishMatchUseCase {
+    private MatchDAO matchDAO;
+
+    public FinishMatchUseCase(MatchDAO matchDAO) {
+        this.matchDAO = matchDAO;
+    }
+
+    public boolean finishMatch(Integer matchId) {
+
+        Match existingMatch = matchDAO.findOne(matchId)
+                .orElseThrow(() -> new EntityNotFoundException("Match not found."));
+
+        if (existingMatch.getConcluded()) {
+            System.out.println("The match has already been concluded.");
+            return false;
+        }
+
+        existingMatch.setConcluded(true);
+
+        return matchDAO.update(existingMatch);
+    }
 }
--- championship/src/main/java/br/edu/ifsp/domain/usecases/match/administration/UpdateMatchScoreUseCase.java
+++ championship/src/main/java/br/edu/ifsp/domain/usecases/match/administration/UpdateMatchScoreUseCase.java
@@ -1,2 +1,22 @@
-package br.edu.ifsp.domain.usecases.match.administration;public class UpdateMatchScoreUseCase {
+package br.edu.ifsp.domain.usecases.match.administration;
+
+import br.edu.ifsp.domain.entities.championship.Match;
+import br.edu.ifsp.domain.usecases.match.crud.MatchDAO;
+import br.edu.ifsp.domain.usecases.utils.EntityNotFoundException;
+
+public class UpdateMatchScoreUseCase {
+    private MatchDAO matchDAO;
+
+    public UpdateMatchScoreUseCase(MatchDAO matchDAO) {
+        this.matchDAO = matchDAO;
+    }
+
+    public boolean updateMatchScore(Integer matchId, Integer newScoreboard1, Integer newScoreboard2) {
+        Match existingMatch = matchDAO.findOne(matchId).orElseThrow(() -> new EntityNotFoundException("Match not found."));
+
+        existingMatch.setScoreboard1(newScoreboard1);
+        existingMatch.setScoreboard2(newScoreboard2);
+
+        return matchDAO.update(existingMatch);
+    }
 }
