3ca0a046b73dcb87318dd3c85fe86dbaa1b34e0e
GiovaneGuimaraes <gioguima24@gmail.com>
2023-12-09 15:44:48 -0300

    telas pontos corridos

---
+++ championship/src/main/java/br/edu/ifsp/application/main/controller/PontosCorridosManageController.java
@@ -0,0 +1,14 @@
+package br.edu.ifsp.application.main.controller;
+
+import javafx.event.ActionEvent;
+
+public class PontosCorridosManageController {
+    public void nextScene(ActionEvent actionEvent) {
+    }
+
+    public void ExportClassificacao(ActionEvent actionEvent) {
+    }
+
+    public void previousScene(ActionEvent actionEvent) {
+    }
+}
---
+++ championship/src/main/java/br/edu/ifsp/application/main/controller/viewPartidaPontosCorridosController.java
@@ -0,0 +1,11 @@
+package br.edu.ifsp.application.main.controller;
+
+import javafx.event.ActionEvent;
+
+public class viewPartidaPontosCorridosController {
+    public void nextScene(ActionEvent actionEvent) {
+    }
+
+    public void previousScene(ActionEvent actionEvent) {
+    }
+}
