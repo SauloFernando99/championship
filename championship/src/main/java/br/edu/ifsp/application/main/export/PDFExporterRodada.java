8a84bc515ef3fb7b2c29d0a50ebb65119b45df1b
Pedro-Avz <pedroalvarez96@hotmail.com>
2023-12-13 11:25:46 -0300

    :bug: fix: import apache

---
+++ championship/src/main/java/br/edu/ifsp/application/main/export/PDFExporterRodada.java
@@ -0,0 +1,10 @@
+package br.edu.ifsp.application.main.export;
+
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import javafx.collections.ObservableList;
+
+public class PDFExporterRodada {
+}
